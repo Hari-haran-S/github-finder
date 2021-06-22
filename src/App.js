13cb33c4d96fafa4d16aabc1c9a5466a3902c3b3
Hari-haran-S <59164672+Hari-haran-S@users.noreply.github.com>
2021-06-22 09:02:31 +0530

    Functional Component implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
+import React, {Fragment,useState} from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import axios from 'axios' 
 import './App.css';
 
-function App() {
+
+const App = () => {
+    const [users,setUsers] =    useState([]);
+    const [user,setUser] =      useState({});
+    const [repos,setRepos] =    useState([]);
+    const [loading,setLoading] =useState(false);
+    const [alert,setAlert] =    useState(null);
+
+    const searchUsers = async text => {
+      setLoading(true);
+      const res = await axios.get(
+        `https://api.github.com/search/users?q=${text}&
+        client_id=${process.env.REACT_APP_GH_CLID}&client_secret=${process.env.REACT_APP_GH_CLSEC}`
+      );
+      setUsers(res.data.items)
+      setLoading(false)
+    }
+
+    const getUser = async userame => {
+      setLoading(true)
+      const res = await axios.get(
+        `https://api.github.com/users/${userame}?
+        client_id=${process.env.REACT_APP_GH_CLID}&client_secret=${process.env.REACT_APP_GH_CLSEC}`
+      );
+      setUser(res.data)
+      setLoading(false)
+    }
+
+    const getUserRepos = async userame => {
+      setLoading(true)
+      const res = await axios.get(
+        `https://api.github.com/users/${userame}/repos?per_page=5&sort=created:asc&
+        client_id=${process.env.REACT_APP_GH_CLID}&client_secret=${process.env.REACT_APP_GH_CLSEC}`
+      );
+      setRepos(res.data)
+      setLoading(false)
+    }
+    const clearUsers = () => {
+      setUsers([])
+      setLoading(false)
+    }
+    const putAlert = (msg, type) => {
+      setAlert({msg,type});
+      setTimeout(() => setAlert(null),3000)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Alert alert={alert}/>
+          <Switch>
+            <Route exact path='/' render={props => (
+              <Fragment>
+                <Search searchUsers={searchUsers}
+                    clearUsers={clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    putAlert={putAlert} />
+                <Users loading={loading} users={users} />
+              </Fragment>
+            )}
+          />
+          <Route exact path= '/about' component={About} />
+          <Route exact path='/user/:login' render={props => (
+            <User 
+              {...props}
+              getUser={getUser}
+              getUserRepos={getUserRepos}
+              user={user}
+              repos={repos}
+              loading={loading}
+            />
+            )}
+          />
+          </Switch>
+        </div>
+        
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/components/users/Search.js
@@ -0,0 +1,46 @@
+import React  ,{ useState }from 'react'
+
+
+const Search = ({putAlert,searchUsers,showClear,clearUsers}) => {
+    const [text,setText] = useState('')
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if (text === '') {
+          putAlert('Please enter something', 'light');
+        } else {
+          searchUsers(text);
+          setText('');
+        }
+      };
+  const onChange = e => setText(e.target.value);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className='form'>
+        <input
+          type='text'
+          name='text'
+          placeholder='Search Users...'
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type='submit'
+          value='Search'
+          className='btn btn-dark btn-block'
+        />
+      </form>
+      {showClear && (
+        <button
+          className='btn btn-light btn-block'
+          onClick={clearUsers}
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Search
